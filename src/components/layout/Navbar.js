c60efe337e5ae348e22924d10603229851709674
sachin barali <sacheenbarali41@gmail.com>
2022-04-17 11:55:26 +0545

    second react course

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,45 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './navbar.css'
 
 const Navbar = () => {
-  return (
-    <>
-        <div className="row">
-          <div className="col-md-3">
-          <Link className="navbar-brand" to="#">Navbar</Link>
-          </div>
-          <div className="col-md-6">
-          <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-          </div>
-          <div className="col-md-3">
-            icons
-          </div>
-        </div>
+    return (
+        <>
+            <div className="row pt-1 bg-dark">
+                <div className="col-md-3">
+                    <Link className="navbar-brand" to="#">Navbar</Link>
+                </div>
+                <div className="col-md-6">
+                <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                </div>
+                <div className="col-md-3">
+                    <Link to="#"><i class="bi bi-box-arrow-in-right"></i></Link>
+                    <Link to="#"><i class="bi bi-person-plus-fill"></i></Link>
+                    <Link to="#"><i class="bi bi-box-arrow-in-right"></i></Link>
+                </div>
+            </div>
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-   
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="#">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="#">Link</Link>
-        </li>
-      </ul>
-     
-    </div>
-  </div>
-</nav>
-    </>
-  )
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="#">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="#">Link</Link>
+                            </li>
+                        </ul>
+                       
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
